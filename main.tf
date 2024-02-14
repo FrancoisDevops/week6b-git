2bbd055339a7a81afb146760293775cc67e635b5
Francois <fordeogold@gmail.com>
2024-02-14 19:36:02 +0100

    lightsail

---
+++ main.tf
@@ -0,0 +1,14 @@
+ressource "aws_lighsail_instance" "server1"
+name " dev-server"
+blueprint_id = "ubuntu_18_04
+bundle_id = "medium_1_0"
+availability zone_zone = "us-east-1a"
+user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              echo '<h1>This is deployed by Francois </h1>' | sudo tee /var/www/html/index.html
+              EOF
+              }
